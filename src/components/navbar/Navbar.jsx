7f96b7283781351628cf7c4702c9e31012850a32
AR Ashraf <airriaislam@gmail.com>
2023-03-10 16:14:53 +0600

    Logged in users will see dashboard nav link directly in the nav bar now.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from "../../assets/images/logo.png";
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import {ROOT, LOGIN, OVERVIEW, PRICING} from "../../helpers/Constants";
+import {ROOT, LOGIN, OVERVIEW, PRICING, DASHBOARD} from "../../helpers/Constants";
 import {menuAction} from "../../redux/actions";
 import {AnimatedButton} from '..';
 import { CgMenuRight } from 'react-icons/cg';
@@ -14,7 +14,8 @@ import { OpenLinkNewTab } from '../../helpers/Functions';
 
 const Navbar = () => {
   const dispatch = useDispatch();
-  const isMenu = useSelector(state => state.isMenu)
+  const isMenu = useSelector(state => state.isMenu);
+  const isToken = useSelector(state => state.isToken);
   
 
   
@@ -28,7 +29,14 @@ const Navbar = () => {
         <div className="bguest__navbar-menu">
           <NavLink to={OVERVIEW} preventScrollReset={true} activeClassName="active">Product Overview</NavLink>
           <NavLink to={PRICING} preventScrollReset={true} activeClassName="active">Pricing</NavLink>
+          { 
+          (isToken || localStorage.getItem("saveToken")) 
+          ?
+          <NavLink to={DASHBOARD} preventScrollReset={true} activeClassName="active">Dashboard</NavLink>
+          :
           <NavLink to={LOGIN} preventScrollReset={true} activeClassName="active">Log In</NavLink>
+          }
+          
           <AnimatedButton 
           width="150px" 
           height="60px" 
